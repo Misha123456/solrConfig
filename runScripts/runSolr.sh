3263a7dc626271746195f1c9110868c1d01ea8f2
Mikhail_Asadchy <Mikhail_Asadchy@epam.com>
2017-04-06 10:18:10 +0300

    last changes

--- runScripts/runSolr.sh
+++ runScripts/runSolr.sh
@@ -1,15 +1,23 @@
-SOLR_HOME=/opt/temp/solr
+CURRENT_FOLDER=${PWD}
+
+ROOT_FOLDER=/media/mikhail/fd1ad2f6-4a8c-4de6-9203-5d35f9cdea4a/temp/solrConfig/solrConfig
+
+SOLR_HOME=${ROOT_FOLDER}"/solr"
 SOLR_BIN=${SOLR_HOME}/bin
 SOLR_CLOUD_SCRIPTS=${SOLR_HOME}"/server/scripts/cloud-scripts/"
 
 SOLR_PORT=8983
 
-#. ./runZookeepers.sh
+#cd ${SOLR_BIN}
+#. ./solr stop -p ${SOLR_PORT}
+#cd ${CURRENT_FOLDER}
+. ./stopZookeepers.sh
+. ./runZookeepers.sh
 
 # copy solr.xml
 # copy hybris configs
 cd ${SOLR_BIN}
-. ./solr start -cloud -p ${SOLR_PORT} -s "/opt/temp/solrdata/node1" -z "localhost:2181,localhost:2182,localhost:2183"
+. ./solr start -cloud -p ${SOLR_PORT} -s ${ROOT_FOLDER}"/solrdata/node1" -z "localhost:2181,localhost:2182,localhost:2183"
 
 #cd ${SOLR_CLOUD_SCRIPTS}
 #. ./zkcli.sh -zkhost localhost:2181 -cmd upconfig -confdir "/opt/temp/conf" -confname default
--- runScripts/stopZookeepers.sh
+++ runScripts/stopZookeepers.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 currentFolder=${PWD}
 
-pathToNode1="/opt/temp/z1/bin"
-pathToNode2="/opt/temp/z2/bin"
-pathToNode3="/opt/temp/z3/bin"
+pathToNode1="../z1/bin"
+pathToNode2="../z2/bin"
+pathToNode3="../z3/bin"
 
 cd ${pathToNode1}
 gnome-terminal -e "./zkServer.sh stop" --profile=Default
+cd ${currentFolder}
 
 cd ${pathToNode2}
 gnome-terminal -e "./zkServer.sh stop" --profile=Default
+cd ${currentFolder}
 
 cd ${pathToNode3}
 gnome-terminal -e "./zkServer.sh stop" --profile=Default
+cd ${currentFolder}
 
 cd ${currentFolder}
